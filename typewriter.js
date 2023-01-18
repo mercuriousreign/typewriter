5f6640ff426dbc4ced5019f99d0d393c8427d5cc
Zaima Atoshi <zaimaz.atoshi@gmail.com>
2023-01-18 14:07:26 -0500

    Typewriter animation using setTimeout

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let time = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {process.stdout.write(char);},time);
+  time += 50;
+}
+
+setTimeout(() => { process.stdout.write("\n");},time);
